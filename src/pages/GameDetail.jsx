7b89034cd983b071ae9179b0e0c277ddb14f1a31
Elegm92 <elegm92@gmail.com>
2026-05-11 20:49:29 +0200

    feat: game detail con galería de imágenes y navegación

--- src/pages/GameDetail.jsx
+++ src/pages/GameDetail.jsx
@@ -1,8 +1,38 @@
 // src/pages/GameDetail.jsx
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { getGameById } from "../services/api";
+
 function GameDetail() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [game, setGame] = useState(null);
+
+  useEffect(() => {
+    getGameById(id)
+      .then((data) => setGame(data))
+      .catch((err) => console.error(err));
+  }, [id]);
+
+  if (!game) return <p>Cargando...</p>;
+
+
   return (
     <div>
-      <h1>Game Detail</h1>
+      <button onClick={() => navigate(-1)}>← Volver</button>
+      <h2>{game.name}</h2>
+      {game.media.type === "gallery" ? (
+        game.media.images.map((img, index) => (
+          <img key={index} src={img} alt={game.name} width="200" />
+        ))
+      ) : (
+        <img src={game.media.image} alt={game.name} width="400" />
+      )}
+      <p>Rating: {game.rating}</p>
+      <p>Géneros: {game.genres.join(", ")}</p>
+      <p>Plataformas: {game.platforms.join(", ")}</p>
+      <p>Fecha de lanzamiento: {game.released}</p>
+      <p>{game.description}</p>
     </div>
   );
 }
