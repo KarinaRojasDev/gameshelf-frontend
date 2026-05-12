882c93b4869a88e79b490f253b8692cb459141a7
Elegm92 <elegm92@gmail.com>
2026-05-12 12:14:00 +0200

    feat: header oculto en login y register

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { Link, useNavigate, useLocation  } from "react-router-dom";
+import useAuth from "../context/useAuth.js";
+
+function Header() {
+  const navigate = useNavigate();
+  const { user, logout } = useAuth();
+  const location = useLocation()
+
+  
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/login");
+  };
+
+  if (location.pathname === "/login" || location.pathname === "/register") {
+    return null;
+  }
+  return (
+    <header>
+      <Link to={user ? "/home" : "/login"}>Home</Link>
+      <nav>
+        {user ? (
+          <>
+            <Link to={`/profile/${user._id}`}>Mi perfil</Link>
+            <button type="button" onClick={handleLogout}>
+              Cerrar sesión
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
